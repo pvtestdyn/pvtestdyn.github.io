12d7b59d5c1b10605d07de3796ceb4717690574a
Pietro Verde <pietroverde@gmail.com>
2022-04-28 18:22:12 +0100

    del 2

--- js/reqpage.js
+++ js/reqpage.js
@@ -1,5 +1,7 @@
 
-
+/**
+ * prototype, work in progress.
+ */
 class Main{
     constructor(){
         this.MAX_AUDIENCES = 10;
@@ -7,6 +9,9 @@ class Main{
         this.audList = [];
     }
     newAudience(){
+        if(this.audienceIndex==0){
+            document.querySelector("#submit").style.display="inline-block";
+        }
         let audience = new Audience(this.audienceIndex);
         this.audList.push(audience);
         this.audienceIndex++;
@@ -23,6 +28,8 @@ buildBtn.addEventListener("click", function(){
 class Audience{
     constructor(audID){
         this.audNumber = audID;
+        this.audName = "prova";
+        this.country="";
         this.ageFrom = 18;
         this.ageTo = 99; 
         this.criteriaBoxes = [];
@@ -47,14 +54,49 @@ class Audience{
             let audNameBox = document.createElement("input");
             audNameBox.setAttribute("class", "audiencename-box");
             audNameBox.setAttribute("id", "audiencename-box_"+this.audNumber);
+            audNameBox.setAttribute("aud-id",this.audNumber);
             audNameBox.setAttribute("placeholder","Audience Name");
             headerAudience.appendChild(audNameBox);
 
-            let countryBtn = document.createElement("button");
-            countryBtn.setAttribute("class","country-btn");
-            countryBtn.setAttribute("id", "country-btn_"+this.audNumber);
-            countryBtn.innerHTML = "Country";
-            headerAudience.appendChild(countryBtn);
+            audNameBox.addEventListener("input",function(){
+                let audID = this.getAttribute("aud-id");
+                let audience = main.audList[audID];
+                audience.setName(this.value);
+            })
+
+
+            let countrySelect = document.createElement("select");
+            countrySelect.setAttribute("class","country-select");
+            countrySelect.setAttribute("aud-id",this.audNumber)
+            countrySelect.setAttribute("id", "country-select_"+this.audNumber);
+            countrySelect.innerHTML = "Country";
+            headerAudience.appendChild(countrySelect);
+
+                let cLabel = document.createElement("option");
+                cLabel.setAttribute("class", "country-label");
+                cLabel.setAttribute("value", "");
+                cLabel.setAttribute("disabled", true);
+                cLabel.setAttribute("selected", true);
+                cLabel.setAttribute("hidden", true);
+                cLabel.innerText = "Country";
+                countrySelect.appendChild(cLabel);
+
+                let countryList = ["UK", "France" , "Germany", "Italy", "Spain"];
+                for (let i= 0 ; i < countryList.length; i++){
+                    let opC = document.createElement("option");
+                    opC.setAttribute("class", "country-option");
+                    opC.setAttribute("id", "country-option_"+countryList[i]);
+                    opC.innerText = countryList[i];
+                    countrySelect.appendChild(opC);
+                }
+                
+            countrySelect.addEventListener("input",function(){
+                let audID = this.getAttribute("aud-id");
+                let audience = main.audList[audID];
+                audience.setCountry(this.value);
+            })
+
+
 
             let expandBtn = document.createElement('button');
             expandBtn.setAttribute("id", "expandAudBtn_"+this.audNumber);
@@ -72,15 +114,29 @@ class Audience{
 
             let ageFromBox = document.createElement("input");
             ageFromBox.setAttribute("class","ageBox");
+            ageFromBox.setAttribute("aud-id",this.audNumber);
             ageFromBox.setAttribute("placeholder","18");
             ageFromBox.setAttribute("id","ageFromBox");
             innerBox.appendChild(ageFromBox);
 
+            ageFromBox.addEventListener("input",function(){
+                let audID = this.getAttribute("aud-id");
+                let audience = main.audList[audID];
+                audience.setAgeFrom(this.value);
+            })
+
             let ageToBox = document.createElement("input");
             ageToBox.setAttribute("class","ageBox");
+            ageToBox.setAttribute("aud-id",this.audNumber);
             ageToBox.setAttribute("placeholder","99");
             ageToBox.setAttribute("id","ageToBox");
             innerBox.appendChild(ageToBox);
+
+            ageToBox.addEventListener("input",function(){
+                let audID = this.getAttribute("aud-id");
+                let audience = main.audList[audID];
+                audience.setAgeTo(this.value);
+            })
 
             let addCriteriaBtn = document.createElement('button');
             addCriteriaBtn.setAttribute("class","addCriteriaBtn");
@@ -93,6 +149,7 @@ class Audience{
                 let audIdx = addCriteriaBtn.value;
                 let audience = main.audList[audIdx];
                 let criteria = new Criteria(audience)
+                audience.criteriaBoxes.push(criteria);
                 audience.incrementCriteriaIdx();
             })
         
@@ -103,12 +160,34 @@ class Audience{
     incrementCriteriaIdx(){
         this.criteriaCount++;
     }
+    setName(text){
+        this.audName = text;
+    }
+    setCountry(c){
+        this.country = c;
+    }
+    setAgeFrom(n){
+        this.ageFrom = n;
+    }
+    setAgeTo(n){
+        this.ageTo = n;
+    }
+    getAudienceID(){
+        return this.audNumber;
+    }
 }
 
+
+/**
+ * Criteria class.
+ */
 class Criteria{
     constructor(audience){
         this.audNumber = audience.audNumber;
         this.criNumber = audience.criteriaCount;
+        this.attributeList = [];
+        this.PageTrayItemsIDList = []; // ids attributes in tray
+        this.PageOptionItemsIDList = []; // ids of attributes in dropdown .. maybe not needed
         this.createCriteriaContainer();
     }
     /**
@@ -124,7 +203,11 @@ class Criteria{
         criteriaBox.setAttribute("id","CRITERIABOXaud_"+this.audNumber+"-cri_"+this.criNumber);
         criteriaBox.setAttribute("class","criteriaBox");
         // remember "any of"
-        criteriaBox.innerHTML = "<p>"+"CRITERIABOX-aud_"+this.audNumber+"-cri_"+this.criNumber+"</p>";
+        let anyOfText = "Any of:";
+        if (this.criNumber>0){
+            anyOfText = "and and of:"
+        }
+        criteriaBox.innerHTML = "<p>"+anyOfText+"</p>";
          
         /*
          * the audience expand button is displayed at this point instead of when the 
@@ -143,6 +226,8 @@ class Criteria{
             // create tray
             let tray = document.createElement("div");
             tray.setAttribute("class", "tray");
+            tray.setAttribute("aud-id",this.audNumber);
+            tray.setAttribute("cri-id",this.criNumber);
             tray.setAttribute("id","TRAY-aud_"+this.audNumber+"-cri_"+this.criNumber);
             headerCriteria.appendChild(tray);
 
@@ -174,43 +259,46 @@ class Criteria{
             selectItem.setAttribute("id","select-attributes_aud-"+this.audNumber+"-cri_"+this.criNumber)
             contentCriteria.append(selectItem);
 
-            // populate the attribute list
-            selectItem.innerHTML = `
-            <optionclass="attribute">mela</option>
-            <option class="attribute">prova1</option>
-            <option class="attribute">banana</option>
-            <option class="attribute">prova2</option>
-            <option class="attribute">mela</option>
-            <option class="attribute">pera</option>
-            <option class="attribute">prova1</option>
-            <option class="attribute">banana</option>
-            <option class="attribute">prova2</option>
-            <option class="attribute">mela</option>
-            <option class="attribute">pera</option>
-            <option class="attribute">prova1</option>
-            <option class="attribute">banana</option>
-            <option class="attribute">prova2</option>
-            <option class="attribute">mela</option>
-            <option class="attribute">pera</option>
-            <option class="attribute">prova1</option>
-            <option class="attribute">banana</option>
-            <option class="attribute">prova2</option>
-            <option class="attribute">mela</option>
-            <option class="attribute">pera</option>
-            <option class="attribute">prova1</option>
-            <option class="attribute">banana</option>
-            <option class="attribute">prova2</option>
-            <option class="attribute">mela</option>
-            <option class="attribute">pera</option>
-            <option class="attribute">prova1</option>
-            <option class="attribute">banana</option>
-            <option class="attribute">prova2</option>
-            <option class="attribute">mela</option>
-            <option class="attribute">pera</option>
 
-            `
+            for (let i = 0; i < attributesFromFile.length; i++){
+                let attr = attributesFromFile[i];
 
-        // add selection event listener to the attribute list
+              
+
+                
+                let op = document.createElement("option");
+                
+                let opID = "aud_"+this.audNumber+"-cri_"+this.criNumber+"-attr"+attr["key"];
+                op.setAttribute("class","attribute");
+                op.setAttribute("key", attr["key"]);
+                op.setAttribute("id", opID);
+                    
+
+                let p = document.createElement("p");
+                p.setAttribute("class","attributeP");
+                op.appendChild(p);
+
+                let spanQ = document.createElement("span");
+                spanQ.setAttribute("class","parameter-name");
+                spanQ.innerHTML = attr["paramname"]+": ";
+                p.appendChild(spanQ);
+
+                let spanA = document.createElement("span");
+                spanA.setAttribute("class","answer-text");
+                spanA.innerHTML = attr["answertext"];
+                p.appendChild(spanA);
+
+
+                selectItem.appendChild(op);
+
+   
+      
+                        
+                
+            }
+                
+           
+           
         selectItem.addEventListener("change", function(){
             selectAttribute(selectItem, tray);
         });
@@ -226,16 +314,68 @@ class Criteria{
         
         audienceContainer.append(criteriaBox);
     }
+
+    /**
+     * Removes item from attributeList.
+     * @param {*} item item to remove.
+     */
+    removeAttributeFromList(item){
+        let l = this.attributeList;
+        l.splice(l.indexOf(item), 1);
+    }
 }
 
 
+/**
+ * Add selection event listener to the attribute list.
+ * On attribute selection, add it to tray and store it in attribute list.
+ * @param {*} selectElem 
+ * @param {*} tray 
+ */
 function selectAttribute(selectElem, tray) {
     let option = selectElem.options[selectElem.selectedIndex];
+
+    let audID = tray.attributes["aud-id"].value;
+    let criID = tray.attributes["cri-id"].value;
+    let audience = main.audList[audID];
+    let criteria = audience.criteriaBoxes[criID];
+    let attrID = option.attributes["key"].value;
+
+   
+    // remove from list
     option.style.display = 'none';
-    let toAdd = document.createElement('p');
-    toAdd.innerHTML = option.text;
+    
+    // add it to tray
+    let toAdd = document.createElement('div');
+    toAdd.setAttribute("class","attr-in-tray");
+    toAdd.setAttribute("aud-id", audID);
+    toAdd.setAttribute("cri-id", criID);
+    toAdd.setAttribute("attr-id",attrID);
+    toAdd.setAttribute("id","attr-in-tray-aud_"+audID+"-cri_"+criID+"-attr_"+attrID);
+    let p = document.createElement('p');
+    p.innerHTML = option.text;
+    p.style.display="inline-block";
+    toAdd.appendChild(p);
+
+    let deleteAttrBtn = document.createElement("button");
+    deleteAttrBtn.setAttribute("class","delete-attr-btn");
+    deleteAttrBtn.setAttribute("aud-id", audID);
+    deleteAttrBtn.setAttribute("cri-id", criID);
+    deleteAttrBtn.setAttribute("attr-id",attrID);
+    deleteAttrBtn.innerText="x";
+    deleteAttrBtn.style.display="inline-block";
+    toAdd.appendChild(deleteAttrBtn);
+
     tray.appendChild(toAdd);
     toAdd.style.display="inline-block";
+
+    // add it to the criteria object
+    criteria.attributeList.push(attrID);
+
+
+    deleteAttrBtn.addEventListener("click", function(){
+        deleteFromTray(deleteAttrBtn);
+    })
 }
 
 
@@ -272,79 +412,28 @@ function expand(elem, button) {
 }
 
 
+function deleteFromTray(btn){
+    let audID = btn.attributes["aud-id"].value;
+    let criID = btn.attributes["cri-id"].value;
+    let attrID = btn.attributes["attr-id"].value;
+    let audience = main.audList[audID];
+    let criteria = audience.criteriaBoxes[criID];
+    let itemOnPage = document.querySelector("#attr-in-tray-aud_"+audID+"-cri_"+criID+"-attr_"+attrID);
+    itemOnPage.remove();
+    criteria.removeAttributeFromList(attrID);
+
+    // make the option reappear in the dropdown list
+    let op = document.querySelector("#"+"aud_"+audID+"-cri_"+criID+"-attr"+attrID)
+    op.style.display = 'block';
+}
 
 
-// function createCriteriaContainer(audIdx){
-//     let audience = main.audList[audIdx];
-//     let critIdx = audience.criteriaCount;
-//     let audienceContainer = document.querySelector("#aud-inner-box_"+audIdx);
-//     let criteriaBox = document.createElement('div');
-//     criteriaBox.setAttribute("id","criteria_"+critIdx);
-//     criteriaBox.innerHTML = "criteria "+critIdx;
-//     audienceContainer.append(criteriaBox);
-//     audience.incrementCriteriaIdx();
-//     let expandBtn = document.querySelector("#expandAudBtn_"+audIdx);
-//     expandBtn.style.display="block";
-// }
+function submit(){
+    let surveyurl = "https://survey-d.dynata.com/survey/selfserve/53c/2204320?debug=respview&audience=";
+    let finObj = JSON.stringify(main);
+    location.href = surveyurl+finObj;
 
+}
 
-
-// function expand(elem, button) {
-    
-
-//   if (elem.style.display == "" || elem.style.display == "none") {
-//     elem.style.display = "block";
-//     button.innerHTML = "▼";
-//   } else {
-//     elem.style.display = "none";
-//     button.innerHTML = "▲";;
-//   }
-  
-// }
-
-// function expand(elem, button) {
-
-//     if (elem.className == "open") {
-//       elem.className = "";
-//     button.innerHTML = "▼";
-//   } else {
-//    elem.className = "open";
-//     button.innerHTML = "▲";//"Show less";
-//   }
-  
-// }
-
-
-
-
-
-
-
-// function selection() {
-//     var select = document.getElementById('attributeList');
-//     var option = select.options[select.selectedIndex];
-//     console.log(option.value);
-//     console.log(option.text);
-//     option.style.display = 'none';
-//     let el = document.createElement('p');
-//     el.innerHTML = option.text;
-//     let b = document.getElementById("tray");
-//     b.appendChild(el);
-//     el.style.display="inline-block";
-// }
-
-// function toggle(el){
-//     let attrList = document.getElementById(el);
-//     let state = attrList.style.display;
-//     if(state == 'none' || state == '')
-//         attrList.style.display="block";
-//     else
-//         attrList.style.display="none";
-
-// }
-
-// function toggleAttributes(){
-//     toggle("attributeList");
-//     toggle("searchboxInput");
-// }
-
+const submitBtn = document.querySelector("#submit");
+submitBtn.addEventListener("click", submit);
